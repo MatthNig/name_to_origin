708ee9d08c559b7a59c0052b653c0507544ac6b5
MatthNig <matthias.niggli@unibas.ch>
2021-02-17 17:33:26 +0100

    WIP: > retrieved NamePrism predictions for a random sample of athletes names. Use this to determine optimal classification thresholds for NamePrism predictions.

--- Code/training_data/API_verification_sample.R
+++ Code/training_data/API_verification_sample.R
@@ -3,7 +3,7 @@
 #                 sample consisting of olympic athletes name's which  #
 #                 are labeled to ethnic origins.                      #
 # Authors:        Matthias Niggli/CIEB UniBasel                       #
-# Date:           24.01.2021                                          #
+# Date:           17.02.2021                                          #
 #######################################################################
 
 #######################################
@@ -30,7 +30,7 @@ set.seed(24012021)
 df_olympic <- read.csv("Data/athlete_sample.csv")
 
 ## sample names for name-prism -------
-test_sample <- sample_n(df_olympic, size = 5000)
+test_sample <- sample_n(df_olympic, size = 3000)
 test_sample$full_name_encoded <- gsub(" ", "%20", x = test_sample$Name)
 
 ###########################
@@ -38,32 +38,38 @@ test_sample$full_name_encoded <- gsub(" ", "%20", x = test_sample$Name)
 ###########################
 
 ## specify the API-Token and URL -----------------------------------------------
-API_nameprism <- "xxxxxxxxx"
+API_nameprism <- "8cf1f0d395a1daac"
 pred_type = "nat"
 response_format = "json"
 
 # get the 39 different origins from the API
-for(i in 4:42){test_sample[, i] <- NA}
+for(i in 5:43){test_sample[, i] <- NA}
 api_url <- paste("http://www.name-prism.com/api_token/",
                  pred_type, "/", response_format, "/",
                  API_nameprism, "/",
                  "test%20name", sep = "")
 origins <- names(fromJSON(txt = api_url))
-names(test_sample)[4:42] <- origins
+names(test_sample)[5:43] <- origins
 print("Prepared dataframe to store origin predictions from name-prism.")
 
 ## NOT RUN: Get name-prism predictions for all names
-for(i in 1:nrow(test_sample)){
+
+for(i in 2665:nrow(test_sample)){
   api_url <- paste0("http://www.name-prism.com/api_token/",
                     pred_type, "/", response_format, "/",
                     API_nameprism, "/", test_sample$full_name_encoded[i])
   tmp <- fromJSON(txt = api_url)
   tmp <- unlist(tmp)
-  test_sample[i, 4:42] <- tmp
+  test_sample[i, 5:43] <- tmp
   Sys.sleep(0.35) # without Sys.sleep: ~0.5s per loop
 }
 print(paste("Origin predictions for", nrow(test_sample), "names retrieved"))
-write.csv(x = test_sample, file = "API_verification_sample.csv", row.names = FALSE)
+
+###############################
+## Save the verification set ##
+###############################
+
+write.csv(x = test_sample, file = "Data/API_verification_sample.csv", row.names = FALSE)
 
 
 
